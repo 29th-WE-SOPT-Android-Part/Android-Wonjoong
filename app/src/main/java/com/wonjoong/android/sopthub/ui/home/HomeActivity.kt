078d866c423a03036f9289f8fd75d9da44ef183f
Lee WonJoong <realjoong@gmail.com>
2021-10-07 15:02:54 +0900

    [feat] 홈 화면 로직 구현

--- app/src/main/java/com/wonjoong/android/sopthub/ui/home/HomeActivity.kt
+++ app/src/main/java/com/wonjoong/android/sopthub/ui/home/HomeActivity.kt
@@ -1,5 +1,7 @@
 package com.wonjoong.android.sopthub.ui.home
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.wonjoong.android.sopthub.R
@@ -14,11 +16,19 @@ class HomeActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initViewModel()
+        initGithubButton()
     }
 
     private fun initViewModel() {
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+    }
+
+    private fun initGithubButton() {
+        binding.ivGithub.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/WonJoongLee"))
+            startActivity(intent)
+        }
     }
 }
