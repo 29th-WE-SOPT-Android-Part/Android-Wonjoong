087fa81e967ab233c32bbc717535167ab0833b14
Lee WonJoong <realjoong@gmail.com>
2021-10-16 10:57:43 +0900

    [refactor] 주석 제거

--- app/src/main/java/com/wonjoong/android/sopthub/ui/home/githubinfo/data/GithubData.kt
+++ app/src/main/java/com/wonjoong/android/sopthub/ui/home/githubinfo/data/GithubData.kt
@@ -3,24 +3,6 @@ package com.wonjoong.android.sopthub.ui.home.githubinfo.data
 import androidx.annotation.DrawableRes
 import com.wonjoong.android.sopthub.R
 
-//sealed class GithubData2 {
-//    abstract val viewType: GithubFragmentType
-//
-//    data class FollowerData(
-//        @DrawableRes
-//        val imageSrc: Int,
-//        val name: String,
-//        val description: String,
-//        override val viewType: GithubFragmentType
-//    ) : GithubData()
-//
-//    data class RepositoryData(
-//        val name: String,
-//        val description: String,
-//        override val viewType: GithubFragmentType
-//    ) : GithubData()
-//}
-
 
 data class GithubData(
     val name: String,
