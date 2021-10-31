8f828c1c82a2130b1fc8a5605c99449ddc21ce0e
Lee WonJoong <realjoong@gmail.com>
2021-10-31 23:47:48 +0900

    [feat] 권한 요청 코드 추가

--- app/src/main/java/com/wonjoong/android/sopthub/ui/main/camera/CameraFragment.kt
+++ app/src/main/java/com/wonjoong/android/sopthub/ui/main/camera/CameraFragment.kt
@@ -2,14 +2,19 @@ package com.wonjoong.android.sopthub.ui.main.camera
 
 import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.wonjoong.android.sopthub.R
 import com.wonjoong.android.sopthub.databinding.FragmentCameraBinding
 import com.wonjoong.android.sopthub.util.BaseViewUtil
+import java.util.jar.Manifest
 
 class CameraFragment : BaseViewUtil.BaseFragment<FragmentCameraBinding>(R.layout.fragment_camera) {
     private lateinit var getImageResult: ActivityResultLauncher<Intent>
@@ -22,6 +27,17 @@ class CameraFragment : BaseViewUtil.BaseFragment<FragmentCameraBinding>(R.layout
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initCameraButton()
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                1
+            )
+        }
     }
 
     private fun initResultImageIntent() {
@@ -39,11 +55,17 @@ class CameraFragment : BaseViewUtil.BaseFragment<FragmentCameraBinding>(R.layout
     }
 
     private fun initCameraButton() {
-        binding.btnCamera.setOnClickListener {
-            val intent = Intent()
-            intent.type = "image/*"
-            intent.action = Intent.ACTION_GET_CONTENT
-            getImageResult.launch(intent)
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            binding.btnCamera.setOnClickListener {
+                val intent = Intent()
+                intent.type = "image/*"
+                intent.action = Intent.ACTION_GET_CONTENT
+                getImageResult.launch(intent)
+            }
         }
     }
 }
