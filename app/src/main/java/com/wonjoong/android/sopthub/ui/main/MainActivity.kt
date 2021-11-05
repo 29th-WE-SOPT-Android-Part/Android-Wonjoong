eea8503311d71d5ac59f11d2cc9cbb90eb7e115a
Lee WonJoong <realjoong@gmail.com>
2021-11-05 21:26:00 +0900

    [refactor] 뷰페이저를 내비게이션으로 변경

--- app/src/main/java/com/wonjoong/android/sopthub/ui/main/MainActivity.kt
+++ app/src/main/java/com/wonjoong/android/sopthub/ui/main/MainActivity.kt
@@ -4,47 +4,22 @@ import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.wonjoong.android.sopthub.R
 import com.wonjoong.android.sopthub.databinding.ActivityMainBinding
-import com.wonjoong.android.sopthub.ui.main.adapter.MainViewPagerAdapter
 import com.wonjoong.android.sopthub.util.BaseViewUtil
 
 class MainActivity :
     BaseViewUtil.BaseAppCompatActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.vpMain.adapter = MainViewPagerAdapter(this)
-        binding.vpMain.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                binding.bnvMain.menu.getItem(position).isChecked = true
-            }
-        })
-        binding.bnvMain.setOnItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.profileFragment -> {
-                    binding.vpMain.currentItem = 0
-                    return@setOnItemSelectedListener true
-                }
-                R.id.homeFragment -> {
-                    binding.vpMain.currentItem = 1
-                    return@setOnItemSelectedListener true
-                }
-                else -> {
-                    binding.vpMain.currentItem = 2
-                    return@setOnItemSelectedListener true
-                }
-            }
-        }
+        setNavigation()
     }
 
-    // 추후 다시 navigation graph로 변경할 예정
-//    private fun setNavigation() {
-//        val navHostFragment =
-//            supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
-//        val navController = navHostFragment.findNavController()
-//        binding.bnvMain.setupWithNavController(navController)
-//        binding.bnvMain.isItemHorizontalTranslationEnabled = true
-//    }
+    private fun setNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        binding.bnvMain.setupWithNavController(navController)
+        binding.bnvMain.isItemHorizontalTranslationEnabled = true
+    }
 }
