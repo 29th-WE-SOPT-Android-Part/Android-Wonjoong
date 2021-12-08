5e95389c2b05485aa99c948572fa3c204a4cffa9
Lee WonJoong <realjoong@gmail.com>
2021-12-08 10:42:33 +0900

    [CHORE] Room에서 필요없는 코드 제거

--- data/src/main/java/kr/wonjoong/data/source/local/SoptDatabase.kt
+++ data/src/main/java/kr/wonjoong/data/source/local/SoptDatabase.kt
@@ -2,12 +2,9 @@ package kr.wonjoong.data.source.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 import kr.wonjoong.data.model.AutoLoginData
-import kr.wonjoong.data.source.RoomConverters
 
 @Database(entities = [AutoLoginData::class], version = 1)
-@TypeConverters(RoomConverters::class)
 abstract class SoptDatabase: RoomDatabase() {
     abstract fun soptDAO(): SoptDAO
 }
